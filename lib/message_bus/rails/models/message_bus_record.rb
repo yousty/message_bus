fbc68a69276907e37f20b0dc27c65819f3847d9b
Ivan Dzyzenko <intale.a@gmail.com>
2024-08-22 16:22:27 +0200

    Disable Ar adapter logs by default

--- lib/message_bus/rails/models/message_bus_record.rb
+++ lib/message_bus/rails/models/message_bus_record.rb
@@ -5,9 +5,27 @@ gem 'activerecord', '>= 6' # Require activerecord gem v6+
 module MessageBus
   module Rails
     class MessageBusRecord < ::ActiveRecord::Base
+      module DisableMBLogs
+        def log(...)
+          yield if block_given?
+        end
+      end
+
       self.abstract_class = true
 
       connects_to database: { writing: :message_bus, reading: :message_bus }
+
+      unless ENV['MESSAGE_BUS_ENABLE_LOGS'] == 'true'
+        class << self
+          alias old_connection connection
+
+          def connection
+            old_connection.tap do |conn|
+              conn.singleton_class.include(DisableMBLogs) unless conn.singleton_class < DisableMBLogs
+            end
+          end
+        end
+      end
     end
   end
 end
